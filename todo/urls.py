591f654511e8c2e81e23a4b9a1e4af660bf7b631
Kamil Kalus <kamun99@gmail.com>
2024-02-26 14:22:57 +0100

    Added some basic views and templates which implement basic CRUD operations (without updating)

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', ),
+    path('', views.list_task, name='list-task'),
+    path('add/', views.add_task, name='add-task'),
+    path('complete/<int:task_id>', views.complete_task, name='complete-task'),
+    path('delete/<int:task_id>', views.delete_task, name='delete-task')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
 # Create your views here.
+
+def list_task(request):
+    tasks = Task.objects.all()
+    return render(request, 'todo/list_task.html', {'tasks': tasks})
+
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        Task.objects.create(title=title)
+        return redirect('list-task')
+    return render(request, 'todo/add_task.html')
+
+
+def complete_task(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    task.completed = True
+    task.save()
+    return redirect('list-task')
+
+
+def delete_task(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    task.delete()
+    return redirect('list-task')
